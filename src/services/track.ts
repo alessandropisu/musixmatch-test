457a571fedbce0b5b25d2fad8ed166b784367d1f
Alessandro Pisu <a.pisu@ilivetech.it>
2022-03-18 10:40:17 +0100

    Move API query params into variables

--- src/services/track.ts
+++ src/services/track.ts
@@ -2,13 +2,25 @@ import axiosInstance from "./axios";
 
 // https://developer.musixmatch.com/documentation/api-reference/track-chart-get
 function getTracksApi(tracksNumber = 5) {
-  return axiosInstance.get(
-    `/chart.tracks.get?chart_name=top&page=1&page_size=${tracksNumber}&f_has_lyrics=1`
-  );
+  const params = {
+    chart_name: "top",
+    page: "1",
+    page_size: tracksNumber.toString(),
+    f_has_lyrics: "1",
+  };
+
+  return axiosInstance.get("/chart.tracks.get", {
+    params,
+  });
 }
+
 // https://developer.musixmatch.com/documentation/api-reference/track-snippet-get
-function getTrackSnippetApi(trackId: string) {
-  return axiosInstance.get(`/track.snippet.get?track_id=${trackId}`);
+function getTrackSnippetApi(track_id: string) {
+  return axiosInstance.get("/track.snippet.get", {
+    params: {
+      track_id,
+    },
+  });
 }
 
 export { getTracksApi, getTrackSnippetApi };
